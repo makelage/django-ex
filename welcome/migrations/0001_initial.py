1dd32b72aa01f5133d1e728d8f5fc271fc5d2650
twilight <332851512@qq.com>
2017-11-27 23:08:21 +0800

    init

--- welcome/migrations/0001_initial.py
+++ welcome/migrations/0001_initial.py
@@ -18,4 +18,14 @@ class Migration(migrations.Migration):
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
             ],
         ),
+        migrations.CreateModel(
+            name='SignInfo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('date', models.DateField()),
+                ('item', models.CharField( max_length=100)),
+                ('number', models.IntegerField()),
+            ],
+        ),
     ]
--- welcome/views.py
+++ welcome/views.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# -*- coding: utf-8 -*-
 import os
 from django.shortcuts import render
 from django.conf import settings
